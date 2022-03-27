372a669ca25dbebce6eaf95ab1405413a5be4856
hjs926 <joonsun926@gmail.com>
2022-03-27 23:13:51 +0900

    update

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDEEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
-}
-function handleLinkClick(event) {
-  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add("hidden");
+  // greeting.innerText = "Hello" + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDEEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
