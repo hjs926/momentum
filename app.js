f667776b66691531ecdf3b4babad28b32f25b196
hjs926 <joonsun926@gmail.com>
2022-03-27 15:26:22 +0900

    update

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClcik() {
-  const username = loginInput.value;
-  if (username === "") {
-    alert("please write your name");
-  } else if (username.length > 15) {
-    alert("your name is long");
-  }
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(loginInput.value);
 }
-
-loginButton.addEventListener("click", onLoginBtnClcik);
+loginForm.addEventListener("submit", onLoginSubmit);
